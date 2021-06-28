7a49a7c3ea0a67c57645f304eb0977f7c249e1c1
Gabriel Touron <56262724+GabrielTouron@users.noreply.github.com>
2021-06-28 19:04:23 +0200

    🚧  add snipcart & test basic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,19 +6,41 @@ import type { AppProps } from "next/app";
 import { ReactElement } from "react";
 import { BottomBar } from "../components/BottomBar";
 import { Footer } from "../components/Footer";
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <ChakraProvider theme={theme}>
-      <Box position="relative" min-height="100vh" paddingBottom="250px">
-        <Header />
-        <Container margin="20px auto" maxWidth={{ base: "90%", md: "1100px" }}>
-          <Component {...pageProps} />
-        </Container>
-        <BottomBar />
-        <Footer />
-      </Box>
-    </ChakraProvider>
+    <>
+      <Head key="snipcart">
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Box position="relative" min-height="100vh" paddingBottom="250px">
+          <Header />
+          <Container margin="20px auto" maxWidth={{ base: "90%", md: "1100px" }}>
+            <Component {...pageProps} />
+            <script async src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
+            <div hidden id="snipcart" data-api-key={process.env.NEXT_SNIPCART_API_TOKEN}></div>
+
+            <button
+              className="snipcart-add-item"
+              data-item-id="starry-night"
+              data-item-price="79.99"
+              data-item-url="/paintings/starry-night"
+              data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+              data-item-name="The Starry Night"
+            >
+              Add to cart
+            </button>
+          </Container>
+          <BottomBar />
+          <Footer />
+        </Box>
+      </ChakraProvider>
+    </>
   );
 }
 
