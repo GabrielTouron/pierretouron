b3315e040afaa2099aa3fb76c5dc9fb90b99fba2
Gabriel Touron <gtcode@pm.me>
2023-03-01 15:15:36 +0100

    fix wrong import

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,10 +1,10 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Stack } from "@chakra-ui/layout";
 import { chakra, Flex, useColorMode } from "@chakra-ui/react";
-import router from "next/dist/client/router";
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { NavButton } from "./NavButtonIf";
+import router from "next/router";
 
 export const Nav: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -18,11 +18,6 @@ export const Nav: React.FC = () => {
         <NavButton onClick={() => router.push("/search")} ifHomeBtn={false}>
           Catalogue
         </NavButton>
-        {
-          // <NavButton onClick={() => router.push("/about")} ifHomeBtn={false}>
-          //   À propos
-          // </NavButton>
-        }
 
         {colorMode === "dark" ? (
           <NavButton onClick={toggleColorMode} ifHomeBtn={false}>
@@ -32,7 +27,8 @@ export const Nav: React.FC = () => {
           <NavButton onClick={toggleColorMode} ifHomeBtn={false}>
             <MoonIcon />
           </NavButton>
-        )}
+        )
+        }
         <NavButton className="snipcart-checkout snipcart-summary" ifHomeBtn={false}>
           {
             <>
