8ab8e2866160a647c61d8c787cabb6449015a4ef
Gabriel Touron <gtcode@pm.me>
2023-03-02 12:09:10 +0100

    fix display product

--- domain/product/displayProducts.ts
+++ domain/product/displayProducts.ts
@@ -1,30 +1,25 @@
 import { useRouter } from "next/router";
 import { ProductsFragment } from "./../../graphql/generated";
-import { useState, useEffect } from "react";
 
-export const useDisplayProduct = (allProducts: ProductsFragment[]): ProductsFragment[] => {
+export const getFilteredProducts = (allProducts: ProductsFragment[]): ProductsFragment[] => {
   const router = useRouter();
   const { category, sort } = router.query;
-  const [productList, setProductList] = useState<ProductsFragment[]>(allProducts);
 
-  useEffect(() => {
-    if (category) {
-      setProductList(allProducts.filter((p: ProductsFragment) => p.categories[0].name === category))
-    } else {
-      setProductList(allProducts)
-    }
+  let productList = allProducts;
 
-    if (sort === "price desc") {
-      setProductList(productList.sort((a: ProductsFragment, b: ProductsFragment) => b.price - a.price))
-    } else if (sort === "price asc") {
-      setProductList(productList.sort((a: ProductsFragment, b: ProductsFragment) => a.price - b.price))
-    } else if (sort === "new") {
-      setProductList(productList.sort((a: ProductsFragment, b: ProductsFragment) => {
-        return new Date(a?.createdAt) < new Date(b?.createdAt) ? 1 : -1;
-      }))
-    } else {
-      setProductList(allProducts)
-    }
-  }, [productList])
+  if (category) {
+    productList = allProducts.filter((p: ProductsFragment) => p.categories[0].name === category)
+  }
+
+  if (sort === "price desc") {
+    productList = productList.sort((a: ProductsFragment, b: ProductsFragment) => b.price - a.price)
+  } else if (sort === "price asc") {
+    productList = productList.sort((a: ProductsFragment, b: ProductsFragment) => a.price - b.price)
+  } else if (sort === "new") {
+    productList = productList.sort((a: ProductsFragment, b: ProductsFragment) => {
+      return new Date(a?.createdAt) < new Date(b?.createdAt) ? 1 : -1;
+    })
+  }
+
   return productList;
 };
